a636f94fa4b2f6231163567f3cc90addc2a38039
chetvertak.g <glebsterchetvertak@gmail.com>
2022-05-23 09:54:48 +0300

    turn servers

--- js/stream.js
+++ js/stream.js
@@ -14,20 +14,52 @@ var isMuted = false;
 
 var iceConfig = {
   iceServers: [
-      { url: 'stun:stun.l.google.com:19302' },
-      { url: 'stun:stun1.l.google.com:19302' },
-      { url: 'stun:stun2.l.google.com:19302' },
-      { url: 'stun:stun3.l.google.com:19302' },
-      { url: 'stun:stun4.l.google.com:19302' },
-      { url: 'stun:stun.ekiga.net' },
-      { url: 'stun:stun.ideasip.com' },
-      { url: 'stun:stun.schlund.de' },
-      { url: 'stun:stun.stunprotocol.org:3478' },
-      { url: 'stun:stun.voiparound.com' },
-      { url: 'stun:stun.voipbuster.com' },
-      { url: 'stun:stun.voipstunt.com' },
+      // { url: 'stun:stun.l.google.com:19302' },
+      // { url: 'stun:stun1.l.google.com:19302' },
+      // { url: 'stun:stun2.l.google.com:19302' },
+      // { url: 'stun:stun3.l.google.com:19302' },
+      // { url: 'stun:stun4.l.google.com:19302' },
+      // { url: 'stun:stun.ekiga.net' },
+      // { url: 'stun:stun.ideasip.com' },
+      // { url: 'stun:stun.schlund.de' },
+      // { url: 'stun:stun.stunprotocol.org:3478' },
+      // { url: 'stun:stun.voiparound.com' },
+      // { url: 'stun:stun.voipbuster.com' },
+      // { url: 'stun:stun.voipstunt.com' },
+      { url: 'turn:turn01.hubl.in?transport=udp' },
+      { url: 'turn:turn02.hubl.in?transport=tcp' },
+      
   ],
 };
+
+// turn:turn01.hubl.in?transport=udp
+// turn:turn02.hubl.in?transport=tcp
+
+// {
+//     url: 'turn:numb.viagenie.ca',
+//     credential: 'muazkh',
+//     username: 'webrtc@live.com'
+// },
+// {
+//     url: 'turn:192.158.29.39:3478?transport=udp',
+//     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+//     username: '28224511:1379330808'
+// },
+// {
+//     url: 'turn:192.158.29.39:3478?transport=tcp',
+//     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+//     username: '28224511:1379330808'
+// },
+// {
+//     url: 'turn:turn.bistri.com:80',
+//     credential: 'homeo',
+//     username: 'homeo'
+//  },
+//  {
+//     url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+//     credential: 'webrtc',
+//     username: 'webrtc'
+// }
 
 /////////////////////////////////////////////
 
