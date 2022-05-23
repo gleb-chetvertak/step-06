d12dc2e7845306db5ca3d537987c5589b74499f6
chetvertak.g <glebsterchetvertak@gmail.com>
2022-05-23 11:13:44 +0300

    turn all

--- js/stream.js
+++ js/stream.js
@@ -33,31 +33,31 @@ var iceConfig = {
         credential: 'muazkh',
         username: 'webrtc@live.com'
       },
-      // {
-      //   url: 'turn:192.158.29.39:3478?transport=udp',
-      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      //   username: '28224511:1379330808'
-      // },
-      // {
-      //   url: 'turn:turn.bistri.com:80',
-      //   credential: 'homeo',
-      //   username: 'homeo'
-      // },
-      // {
-      //   url: 'turn:turn.anyfirewall.com:443?transport=tcp',
-      //   credential: 'webrtc',
-      //   username: 'webrtc'
-      // },
-      // {
-      //   url: 'turn:relay.backups.cz',
-      //   credential: 'webrtc',
-      //   username: 'webrtc'
-      // },
-      // {
-      //   url: 'turn:relay.backups.cz?transport=tcp',
-      //   credential: 'webrtc',
-      //   username: 'webrtc'
-      // },
+      {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      },
+      {
+        url: 'turn:turn.bistri.com:80',
+        credential: 'homeo',
+        username: 'homeo'
+      },
+      {
+        url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
+      },
+      {
+        url: 'turn:relay.backups.cz',
+        credential: 'webrtc',
+        username: 'webrtc'
+      },
+      {
+        url: 'turn:relay.backups.cz?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
+      },
       
   ],
 };
