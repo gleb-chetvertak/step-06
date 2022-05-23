b3439267248fb07384d48493a63503bbed877b2d
chetvertak.g <glebsterchetvertak@gmail.com>
2022-05-23 10:03:41 +0300

    more turn servers

--- js/stream.js
+++ js/stream.js
@@ -26,8 +26,43 @@ var iceConfig = {
       // { url: 'stun:stun.voiparound.com' },
       // { url: 'stun:stun.voipbuster.com' },
       // { url: 'stun:stun.voipstunt.com' },
-      { url: 'turn:turn01.hubl.in?transport=udp' },
-      { url: 'turn:turn02.hubl.in?transport=tcp' },
+      // { url: 'turn:turn01.hubl.in?transport=udp' },
+      // { url: 'turn:turn02.hubl.in?transport=tcp' },
+      {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+      },
+      {
+          url: 'turn:192.158.29.39:3478?transport=udp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+      },
+      {
+          url: 'turn:192.158.29.39:3478?transport=tcp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+      },
+      {
+          url: 'turn:turn.bistri.com:80',
+          credential: 'homeo',
+          username: 'homeo'
+      },
+      {
+          url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+          credential: 'webrtc',
+          username: 'webrtc'
+      },
+      {
+        url: 'turn:relay.backups.cz',
+        credential: 'webrtc',
+        username: 'webrtc'
+      },
+      {
+        url: 'turn:relay.backups.cz?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
+      },
       
   ],
 };
